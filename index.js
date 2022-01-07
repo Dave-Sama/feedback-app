e719d23f506e55725d638d7b0734301710f37551
Dave-Sama <davech1228@gmail.com>
2022-01-07 23:41:03 +0200

    starting to work on oauth

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const app = express();
-
-app.get('/greeting', (req, res) => {
-	res.send({ bye: 'bye!' });
-});
+const passport = require('passport');
+const googleStrategy = require('passport-google-oauth20').Strategy();
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log('Server listening on port ' + port));
